a28908868721a69ed8065a2255a44d307b0e6038
Nagaveena007 <nagaveena.hallikeri@gmail.com>
2022-01-17 15:37:19 +0100

    .

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Profile from "./components/Profile/Profile";
+import Home from "./components/Home/Home";
 function App() {
   return (
     <>
-      <Profile />
+      <Home />
     </>
   );
 }
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,13 @@
+import "../FeaturedPosts/FeaturedPosts";
+import FeaturedPosts from "../FeaturedPosts/FeaturedPosts";
+import Profile from "../Profile/Profile";
+
+const Home = () => {
+  return (
+    <>
+      <Profile />
+      <FeaturedPosts />
+    </>
+  );
+};
+export default Home;
