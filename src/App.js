b2818df774038b07e4723dadb33fa0dea8f8ecc4
SefaBckn <31974256+sefabckn@users.noreply.github.com>
2022-01-17 15:33:43 +0100

    navbar is created

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import MyNav from './components/MyNav/MyNav.jsx'
+import { BrowserRouter} from "react-router-dom";
 function App() {
   return <>
+    <BrowserRouter>
+    <MyNav />
   
+    </BrowserRouter>
   </>;
 }
 
