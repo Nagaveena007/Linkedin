569975e9e23bd313bd77eaaeac58dd561d586b0c
RobertaMatera <materaroberta11@gmail.com>
2022-01-18 11:35:36 +0100

    show more - show less functionality implemented -> sidebar

--- src/components/Sidebar/AlsoViewed.jsx
+++ src/components/Sidebar/AlsoViewed.jsx
@@ -3,9 +3,11 @@ import { Card, Button } from "react-bootstrap";
 import RandomUsersCard from "./RandomUsersCard";
 
 const AlsoViewed = () => {
-
-  const [users, setUsers] = useState([])
-
+  const [users, setUsers] = useState([]);
+  const [contentToShow, setContentToShow] = useState(false);
+  const toggleShowMore = () => {
+    setContentToShow(!contentToShow)
+  }
   useEffect(() => {
     fetchUsers();
   }, []);
@@ -24,8 +26,8 @@ const AlsoViewed = () => {
       if (response.ok) {
         let responseJson = await response.json();
         console.log(responseJson);
-        setUsers(responseJson)
-        return users
+        setUsers(responseJson);
+        return users;
       } else {
         console.log("error on fetching data");
       }
@@ -34,18 +36,40 @@ const AlsoViewed = () => {
     }
   };
 
-
   return (
     <Card id="also-viewed" className="mb-4">
       <Card.Title className="my-3 px-2 pt-2">People also viewed</Card.Title>
-          {
-            users.slice(0,8).map((user) => (
-              <RandomUsersCard src={user.image} alt={user.name} text={user.title} name={user.name} surname={user.surname}/>
-            ))
-          }
-      
-      <Button className="btn-connect card-bottom d-flex justify-content-center align-items-center">
-        Show more <i className="bi bi-chevron-compact-down ml-1"></i>
+
+      {users.slice(0, 5).map((user) => (
+        <RandomUsersCard
+          src={user.image}
+          alt={user.name}
+          text={user.title}
+          name={user.name}
+          surname={user.surname}
+          key={user._id}
+        />
+      ))}
+      {contentToShow && (
+        <div>
+          {users.slice(6, 11).map((user) => (
+            <RandomUsersCard
+              src={user.image}
+              alt={user.name}
+              text={user.title}
+              name={user.name}
+              surname={user.surname}
+              key={user._id}
+            />
+          ))}
+        </div>
+      )}
+      <Button
+        className="btn-connect card-bottom d-flex justify-content-center align-items-center"
+        onClick={() => toggleShowMore()}
+      >
+        {contentToShow ? "Show less" : "Show more"}
+        <i className="bi bi-chevron-compact-down ml-1"></i>
       </Button>
     </Card>
   );
