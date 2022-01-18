d03b8423bbdb89563bd6bbf9accf5d9b8e4e47aa
RobertaMatera <materaroberta11@gmail.com>
2022-01-18 15:07:34 +0100

    fix bugs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import MyNav from './components/MyNav/MyNav.jsx'
-import Profile from './components/Profile/Profile.jsx'
-import {useState} from 'react'
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import MyNav from "./components/MyNav/MyNav.jsx";
+import Profile from "./components/Profile/Profile.jsx";
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import FeaturedPosts from "./components/FeaturedPosts/FeaturedPosts";
 import Home from "./components/Home/Home";
 
@@ -12,15 +12,15 @@ function App() {
   const [userProfile, setUserProfile] = useState({});
 
   return (
-  <>
-    <BrowserRouter>
-    <MyNav userProfile={userProfile} setUserProfile={setUserProfile}/>
-    <Routes>
-      <Route path='/profile' element={<Home userProfile={userProfile} />} />
-    </Routes>
-    {/* <FeaturedPosts /> */}
-    </BrowserRouter>
-  </>
+    <>
+      <BrowserRouter>
+        <MyNav userProfile={userProfile} setUserProfile={setUserProfile} />
+        <Routes>
+          <Route path="/" element={<Home userProfile={userProfile} />} />
+        </Routes>
+        {/* <FeaturedPosts /> */}
+      </BrowserRouter>
+    </>
   );
 }
 export default App;
